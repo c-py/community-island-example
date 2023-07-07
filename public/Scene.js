a0a5f981b806ac82b45dfd478a60ff6ba84dfff0
Adam Hannigan <adamhannigan81@gmail.com>
2023-07-07 15:19:57 +1000

    [FIX] Simplify map specs

--- public/Scene.js
+++ public/Scene.js
@@ -1,10 +1,6 @@
 class ExternalScene extends window.BaseScene {
   constructor() {
     super({
-      name: "test_island",
-      map: {
-        json: "https://sunflower-land.github.io/community-island-example/public/map.json",
-      },
       player: {
         spawn: {
           x: 210,
