121496e3e6e54de77e57c23377e8fff58c8455f9
Adam Hannigan <adamhannigan81@gmail.com>
2023-07-11 11:57:40 +1000

    [FEAT] NPC Interaction

--- public/Scene.js
+++ public/Scene.js
@@ -7,6 +7,45 @@ class ExternalScene extends window.BaseScene {
           y: 280,
         },
       },
+      // map: {
+      //   tilesetUrl: "http://localhost:3002/tilesheet.png",
+      // },
+      // mmo: {
+      //   enabled: true,
+      //   url: "ws://localhost:2567",
+      //   roomId: "community_island",
+      // },
     });
+  }
+
+  create() {
+    super.create();
+
+    const npcClothing = {
+      body: "Goblin Potion",
+      hat: "Pirate Hat",
+      hair: "Buzz Cut",
+      shirt: "SFL T-Shirt",
+      pants: "Farmer Pants",
+      tool: "Pirate Scimitar",
+    };
+
+    this.initialiseNPCs([
+      {
+        x: 240,
+        y: 320,
+        npc: "geoff",
+        clothing: npcClothing,
+        onClick: () => {
+          window.openModal({
+            npc: {
+              name: "Geoff",
+              clothing: npcClothing,
+            },
+            jsx: "Howdy farmer! Welcome to the test community island, stay safe out there.",
+          });
+        },
+      },
+    ]);
   }
 }
